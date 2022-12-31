ee6fb88e4e6c3377be990fa84aa90362d450a723
Nethsara Sandeepa Elvitigala <nethsarasandeepaelvitigala@gmail.com>
2022-12-31 22:55:10 +0530

    fix: only the background lights up with the lightsource, text stay intact

--- components/Util/ThemeOption.vue
+++ components/Util/ThemeOption.vue
@@ -38,14 +38,15 @@ watch(() => props.mousePosition, (newVal) => {
     query: {
         theme: props.option
     }
-}" ref="optionCard"
-        class="theme-option flex flex-col gap-8 items-center justify-center py-8 cursor-pointer transition-all duration-400 ease-in-out bg-[rgba(var(--light),0.02)] hover:bg-[rgba(var(--light),0.1)] hover:bg rounded-xl text-white">
-        <Icon name="mdi:lightbulb-on-outline" v-if="props.option === 'modern&creative'" class="text-7xl text-white" />
+}" ref="optionCard" rel="prefetch"
+        class="flex flex-col gap-8 items-center justify-center py-8 cursor-pointer transition-all duration-400 ease-in-out bg-[rgba(var(--light),0.02)] hover:bg-[rgba(var(--light),0.1)] hover:bg rounded-xl text-white">
+        <Icon name="mdi:lightbulb-on-outline" v-if="props.option === 'modern&creative'"
+            class="text-7xl text-white z-50" />
         <Icon name="material-symbols:square-outline-rounded" v-if="props.option === 'minimalist'"
-            class="text-7xl text-white" />
+            class="text-7xl text-white z-50" />
         <Icon name="fluent-emoji-high-contrast:crown" v-if="props.option === 'traditional'"
-            class="text-7xl text-white" />
-        <p v-html="props.text" class="text-center text-md lg:text-xl text-white">
+            class="text-7xl text-white z-50" />
+        <p v-html="props.text" class="text-center text-md lg:text-xl text-white z-50">
         </p>
     </NuxtLink>
 </template>
@@ -54,6 +55,4 @@ watch(() => props.mousePosition, (newVal) => {
 <style lang="scss" scoped>
 @use "@/assets/scss/variables" as *;
 @use "@/assets/scss/mixins" as *;
-
-.theme-option {}
 </style>
